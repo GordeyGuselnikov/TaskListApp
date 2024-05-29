d7c51d0f2073e540a4e77f101eac866f00231852
Gordey Guselnikov <g.guselnikov@bi.zone>
2024-05-29 16:14:03 +0700

    first commit

---
+++ TaskListApp/TaskListViewController.swift
@@ -0,0 +1,27 @@
+//
+//  TaskListTableViewController.swift
+//  TaskListApp
+//
+//  Created by Guselnikov Gordey on 29.05.24.
+//
+
+import UIKit
+
+class TaskListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+}
